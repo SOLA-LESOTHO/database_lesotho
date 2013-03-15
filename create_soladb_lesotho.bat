b0f32d0ec2718909e952ec2196b867cd67ca8d10
Neil Pullar <neil.pullar@fao.org>
2013-03-15 08:29:51 +0100

    Corrections concerning Cape Lo7 Coordinates
    Removal of references to special srid 40000 used to deal with south oriented coordinate system
    srid 22287 does appear to deal appropriately with Cape LO7 coordinates.
    Added missing zone.xml (sld) file that was missing

---
+++ create_soladb_lesotho.bat
@@ -0,0 +1,117 @@
+@echo off
+
+set psql_path=%~dp0
+set psql_path="%psql_path%psql\psql.exe"
+set host=localhost
+set dbname=sola
+
+set username=postgres
+REM set password=?
+set archive_password=?
+
+set createDB=NO
+
+set testDataPath=test-data\lesotho\
+
+
+set /p host= Host name [%host%] :
+
+set /p dbname= Database name [%dbname%] :
+
+set /p username= Username [%username%] :
+
+REM The Database password should be set using PgAdmin III. When connecting to the database, 
+REM choose the Store Password option. This will avoid a password prompt for every SQL file 
+REM that is loaded. 
+REM set /p password= DB Password [%password%] :
+
+REM set /p archive_password= Test Data Archive Password [%archive_password%] :
+
+echo
+echo
+echo Starting Build at %time%
+echo Starting Build at %time% > build.log 2>&1
+
+echo Creating database...
+echo Creating database... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=sola.sql > build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=test_data.sql >> build.log 2>&1
+
+echo Loading business rules...
+echo Loading SOLA business rules... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=business_rules.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_generators.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_application.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_service.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_ba_unit.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_cadastre_object.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_rrr.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_source.sql >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=br_target_bulkoperation.sql >> build.log 2>&1
+
+echo Loading Lesotho Extensions...
+echo Loading Lesotho Extensions... >> build.log 2>&1
+echo Loading Table Changes... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_table_changes.sql >> build.log 2>&1
+echo Loading Reference Data... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_reference_data.sql >> build.log 2>&1
+echo Loading Spatial Config... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_spatial_config.sql >> build.log 2>&1
+echo Loading Lesotho Business Rules... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_business_rules.sql >> build.log 2>&1
+REM Users, Roles and Agents are loaded in system.sql and party.sql. If lesotho.7z is not available, the following scripts
+REM can be run manually to load development users and Lesotho agents. 
+REM echo Loading Users and Roles... >> build.log 2>&1
+REM %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_users_roles.sql >> build.log 2>&1
+REM echo Loading Lodging Agents... >> build.log 2>&1
+REM %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=extension\lesotho_agents.sql >> build.log 2>&1
+
+
+REM Extract Lesotho data files from the zip package. Note that the lesotho.7z has been stripped out of the
+REM git repository. To obtain access to this file, contact Andrew McDowell - andrew.mcdowell@fao.org.
+echo Extracting Lesotho data files...
+echo Extracting Lesotho data files... >> build.log 2>&1
+%testDataPath%7z.exe e -y -p%archive_password% -o%testDataPath% %testDataPath%lesotho.7z >> build.log 2>&1
+%testDataPath%7z.exe e -y -p%archive_password% -o%testDataPath% %testDataPath%lesothoDev.7z >> build.log 2>&1
+
+REM Load the Lesotho test data. 
+REM Direct standard output to NUL, but capture any errors in the build.log
+echo >> build.log
+echo Loading system schema...
+echo Loading system schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%system.sql >NUL 2>>build.log
+echo Loading cadastre schema...
+echo Loading cadastre schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%cadastre.sql >NUL 2>>build.log
+echo Loading address schema...
+echo Loading address schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%address.sql >NUL 2>>build.log
+echo Loading party schema...
+echo Loading party schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%party.sql >NUL 2>>build.log
+echo Loading administration schema...
+echo Loading administration schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%administration.sql >NUL 2>>build.log
+echo Loading application schema...
+echo Loading application schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%application.sql >NUL 2>>build.log
+echo Loading document schema...
+echo Loading document schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%sample_documents.sql >NUL 2>>build.log
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%standard_documents.sql >NUL 2>>build.log
+echo Loading source schema...
+echo Loading source schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%source.sql >NUL 2>>build.log
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%sample_documents_source.sql >NUL 2>>build.log
+REM Source for standard documents should already be loaded from the database extract (source.sql). 
+REM %psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%standard_documents_source.sql >NUL 2>>build.log
+echo Loading transaction schema...
+echo Loading transaction schema... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%transaction.sql >NUL 2>>build.log
+echo Applying data fixes...
+echo Applying data fixes... >> build.log 2>&1
+%psql_path% --host=%host% --port=5432 --username=%username% --dbname=%dbname% --file=%testDataPath%data-fixes.sql >> build.log 2>&1
+
+echo Finished at %time% - Check build.log for errors!
+echo Finished at %time% >> build.log 2>&1
+pause
---
+++ extension/lesotho_srid.sql
@@ -0,0 +1,9 @@
+﻿insert into spatial_ref_sys(srid, auth_name, auth_srid, srtext, proj4text)
+select 40000, auth_name, 40000, srtext, proj4text
+from spatial_ref_sys 
+where srid = 22287;
+
+
+update spatial_ref_sys set srtext = 
+'PROJCS["Cape / Lo27",GEOGCS["Cape",DATUM["Cape",SPHEROID["Clarke 1880 (Arc)",6378249.145,293.4663077,AUTHORITY["EPSG","7013"]],AUTHORITY["EPSG","6222"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4222"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse Mercator (South Orientated)"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",27],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],AUTHORITY["EPSG","22287"],AXIS["Y",WEST],AXIS["X",SOUTH]]'
+where srid = 40000;
