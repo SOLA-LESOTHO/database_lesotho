bdd3423da7a9415aeac84425d0bc62b2cf972062
Ntsane Mafereka <maferekan@gmail.com>
2013-09-10 12:13:02 +0200

    introduced collection date

---
+++ production_changes/pending/changeset5.sql
@@ -0,0 +1,11 @@
+UPDATE party.party
+   SET  ext_id= btrim(to_char(nextval('party.party_nr_seq'::regclass), '000000'::text))
+   
+CREATE SEQUENCE party.party_nr_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 999999
+  START 100000
+  CACHE 1;
+ALTER TABLE party.party_nr_seq
+  OWNER TO postgres;
