677ae937436ad05826286d48bfe85bf2056ec3a7
Alexander Solovov <solovov@gmail.com>
2013-09-11 23:55:20 +0600

    New business rule created to control registration on lease service to be in the list together with grant new lease service.

--- br_target_application.sql
+++ br_target_application.sql
@@ -438,6 +438,17 @@ INSERT INTO system.br_validation(br_id, target_code, target_application_moment, 
 VALUES ('check-payment', 'application', 'validate', 'critical', 20);
 
 ----------------------------------------------------------------------------------------------------------------------
+INSERT INTO system.br(id, technical_type_code, feedback) 
+VALUES('check-lease-reg-for-new-lease', 'sql', 'Lease registration service must exist on the application.');
+
+INSERT INTO system.br_definition(br_id, active_from, active_until, body) 
+VALUES('check-lease-reg-for-new-lease', now(), 'infinity', 
+'SELECT (CASE WHEN (SELECT COUNT(1) FROM application.service WHERE application_id = #{id} AND request_type_code = ''registerLease'') > 0 
+THEN (SELECT COUNT(1)>0 FROM application.service WHERE application_id = #{id} AND request_type_code = ''regOnLease'') ELSE ''t'' END) AS vl');
+
+INSERT INTO system.br_validation(br_id, target_code, target_application_moment, severity_code, order_of_execution)
+VALUES ('check-lease-reg-for-new-lease', 'application', 'validate', 'critical', 20);
+----------------------------------------------------------------------------------------------------------------------
 UPDATE system.br SET display_name = id WHERE display_name !=id;
 
 ----------------------------------------------------------------------------------------------------
