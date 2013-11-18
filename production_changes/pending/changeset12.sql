302a48d8b5d5cd31dcd24ea12be43dd58b2ec556
Bulara <mbulara@gmail.com>
2013-11-18 13:39:18 +0200

    completed mortgage statistics report

--- production_changes/pending/changeset12.sql
+++ production_changes/pending/changeset12.sql
@@ -1,4 +1,7 @@
-﻿
+﻿-- Function: application.getmortgagestats(character varying, character varying)
+
+-- DROP FUNCTION application.getmortgagestats(character varying, character varying);
+
 CREATE OR REPLACE FUNCTION application.getmortgagestats(fromdate character varying, todate character varying)
   RETURNS SETOF record AS
 $BODY$
@@ -14,19 +17,23 @@ BEGIN
 	sqlSt = 'with result_table as (select count(*) as mortgages, sum(r.amount) as amount from administrative.rrr r 
 	where r.transaction_id in (select tr.id from (transaction.transaction tr
 	join application.service sv on tr.from_service_id = sv.id)) and r.type_code = ''mortgage''
-	and r.registration_date BETWEEN to_date('''|| fromdate || ''',''yyyy-mm-dd'')  and to_date('''|| todate || ''',''yyyy-mm-dd'')
-	and r.status_code = ''current'')
+	and  r.status_code = ''current''
+	and r.registration_date BETWEEN to_date('''|| fromdate || ''',''yyyy-mm-dd'')  and to_date('''|| todate || ''',''yyyy-mm-dd''))
 
 	select mortgages, amount, round(amount/mortgages) as average_amount from result_table';
 
-	FOR rec in EXECUTE sqlSt loop
+	FOR rec in EXECUTE sqlSt LOOP
 		mortgages:= rec.mortgages;
 		amount:= rec.amount;
 		average_amount:= rec.average_amount;
 		
 		select into recToReturn mortgages::integer, amount::float, average_amount::float;        
 		return next recToReturn;
-	end loop;
+	END LOOP;
 END;
 $BODY$
-language plpgsql;
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION application.getmortgagestats(character varying, character varying)
+  OWNER TO postgres;
