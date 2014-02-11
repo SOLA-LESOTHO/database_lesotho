f2e425c8fee688e5a352e2e5e76a20a5d7706e3e
unknown <mbulara@gmail.com>
2014-02-11 10:48:13 +0200

    added changeset 24 and 25 sql scripts for trasnfer lease reporting Introduced TransferLeaseView.java in ApplicationEJB Introduced TransferLeaseTO.java in Boundry Transfer Objects Introduced TransferLeaseBean.java In Clients Beans

--- production_changes/pending/changeset24.sql
+++ production_changes/pending/changeset24.sql
@@ -1,13 +1,13 @@
 ﻿DROP FUNCTION IF EXISTS administrative.transfer_lease_data(varchar,varchar);
 CREATE OR REPLACE FUNCTION administrative.transfer_lease_data(varchar,varchar)
-  RETURNS TABLE(service varchar, gender varchar, kind varchar, amount float, stamp_duty float ,transfer_duty float, registration_fee float) AS
+  RETURNS TABLE(service varchar, gender varchar, kind varchar, status varchar, amount float, stamp_duty float ,transfer_duty float, registration_fee float) AS
 $BODY$
 BEGIN
-	return query execute 'with transfers as (with holders as (with rights as(with apps as (select tr.id transaction_id, ser.id service_id, ser.request_type_code request, application_id
+	return query execute '(with transfers as (with holders as (with rights as(with apps as (select tr.id transaction_id, ser.id service_id, ser.request_type_code request, application_id
 	from transaction.transaction as tr join application.service as ser on tr.from_service_id = ser.id) select  display_value, app.change_user,
 	apps.transaction_id, rrr.id rrr_id, app.status_code,rrr.amount, rrr.stamp_duty, rrr.transfer_duty, rrr.registration_fee
 	from application.application app, administrative.rrr as rrr,application.request_type as rt
-	join apps on rt.code = apps.request where rt.code = ''newOwnership'' 
+	join apps on rt.code = apps.request where rt.code = ''newOwnership''
 	and apps.application_id = app.id and app.status_code = ''approved''
 	and rrr.transaction_id = apps.transaction_id
 	and rrr.registration_date BETWEEN to_date('''|| $1 || ''',''yyyy-mm-dd'')  and to_date('''|| $2 || ''',''yyyy-mm-dd''))
@@ -15,11 +15,11 @@ BEGIN
 	from administrative.party_for_rrr as pfr, rights 
 	where pfr.rrr_id in (select rights.rrr_id))
 
-	select holders.display_value service, gender_code gender, type_code, amount::float, stamp_duty::float, transfer_duty::float, registration_fee::float
+	select holders.display_value service, gender_code gender, type_code, legal_type, amount::float, stamp_duty::float, transfer_duty::float, registration_fee::float
 	from party.party, holders
 	where party.id in (select holders.party_id))
 
-	select * from transfers ';
+	select * from transfers)';
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
--- production_changes/pending/changeset25.sql
+++ production_changes/pending/changeset25.sql
@@ -1,4 +1,49 @@
-﻿DROP FUNCTION IF EXISTS administrative.transfer_lease_report(varchar,varchar);
+﻿DROP FUNCTION IF EXISTS administrative.mortgage_report(varchar,varchar);
+CREATE OR REPLACE FUNCTION administrative.mortgage_report(varchar,varchar)
+  RETURNS record AS
+$BODY$
+DECLARE 
+	service_name  varchar;
+	service_count  integer:=0;
+	males  integer:=0;
+	females integer:=0;
+	entities integer:=0;
+	joint integer:=0;
+	total float:=0.0;
+	stamp float:=0.0;
+	transfer float:=0.0;
+	registration float:=0.0;
+	rec record;
+	result record;
+	sqlSt varchar;
+   
+BEGIN
+
+	select service into service_name from  administrative.mortgage_data($1,$2) limit 1;
+	select count(*) into service_count from  administrative.mortgage_data($1,$2);
+	select count(*) into males from  administrative.mortgage_data($1,$2) where gender = 'male' and status != 'Married in community of property';
+	select count(*) into females from  administrative.mortgage_data($1,$2) where gender = 'female' and status != 'Married in community of property';
+	select count(*) into entities from  administrative.mortgage_data($1,$2) where kind = 'nonNaturalPerson';
+	select count(*) into joint from  administrative.mortgage_data($1,$2) where status = 'Married in community of property';
+	select sum(amount) into total from  administrative.mortgage_data($1,$2);
+	select sum(stamp_duty) into stamp from  administrative.mortgage_data($1,$2);
+	select sum(transfer_duty) into transfer from  administrative.mortgage_data($1,$2);
+	select sum(registration_fee) into registration from  administrative.mortgage_data($1,$2);
+
+	select into result service_name::varchar, service_count::integer, males::integer, females::integer, entities::integer, joint::integer, total::float, stamp::float,
+	transfer::float, registration::float;
+
+	return result;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+
+  /*select * from administrative.mortgage_report('2013-01-01','2014-02-27') AS report(service_name varchar, service_count integer, males integer, females integer,
+  entities integer, joint integer, total_amount float, stamp_duty float,
+	transfer_duty float, registration_fee float);*/
+	
+DROP FUNCTION IF EXISTS administrative.transfer_lease_report(varchar,varchar);
 CREATE OR REPLACE FUNCTION administrative.transfer_lease_report(varchar,varchar)
   RETURNS record AS
 $BODY$
@@ -8,6 +53,7 @@ DECLARE
 	males  integer:=0;
 	females integer:=0;
 	entities integer:=0;
+	joint integer:=0;
 	total float:=0.0;
 	stamp float:=0.0;
 	transfer float:=0.0;
@@ -20,15 +66,16 @@ BEGIN
 
 	select service into service_name from administrative.transfer_lease_data($1,$2) limit 1;
 	select count(*) into service_count from administrative.transfer_lease_data($1,$2);
-	select count(*) into males from administrative.transfer_lease_data($1,$2) where gender = 'male';
-	select count(*) into females from administrative.transfer_lease_data($1,$2) where gender = 'female';
+	select count(*) into males from administrative.transfer_lease_data($1,$2) where gender = 'male' and status != 'Married in community of property';
+	select count(*) into females from administrative.transfer_lease_data($1,$2) where gender = 'female' and status != 'Married in community of property';
 	select count(*) into entities from administrative.transfer_lease_data($1,$2) where kind = 'nonNaturalPerson';
+	select count(*) into joint from administrative.transfer_lease_data($1,$2) where status = 'Married in community of property';
 	select sum(amount) into total from administrative.transfer_lease_data($1,$2);
 	select sum(stamp_duty) into stamp from administrative.transfer_lease_data($1,$2);
 	select sum(transfer_duty) into transfer from administrative.transfer_lease_data($1,$2);
 	select sum(registration_fee) into registration from administrative.transfer_lease_data($1,$2);
 
-	select into result service_name::varchar, service_count::integer, males::integer, females::integer, entities::integer, total::float, stamp::float,
+	select into result service_name::varchar, service_count::integer, males::integer, females::integer, entities::integer, joint::integer, total::float, stamp::float,
 	transfer::float, registration::float;
 
 	return result;
@@ -37,6 +84,12 @@ $BODY$
   LANGUAGE plpgsql VOLATILE;
 
 
-  select * from administrative.transfer_lease_report('2013-01-01','2014-02-27') AS report(service_name varchar, service_count integer, males integer, females integer,
-  entities integer, total_amount float, stamp_duty float,
+ /*select * from administrative.transfer_lease_report('2013-01-01','2014-02-27') AS report(service_name varchar, service_count integer, males integer, females integer,
+  entities integer, joint integer, total_amount float, stamp_duty float,
+	transfer_duty float, registration_fee float) union
+
+select * from administrative.mortgage_report('2013-01-01','2014-02-27') AS report(service_name varchar, service_count integer, males integer, females integer,
+  entities integer, joint integer, total_amount float, stamp_duty float,
 	transfer_duty float, registration_fee float)
+*/
+
