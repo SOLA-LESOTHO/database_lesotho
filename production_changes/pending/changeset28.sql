f9ce47eadab80d032d0692a445b766e2c5e8f85b
unknown <mbulara@gmail.com>
2014-03-27 09:11:40 +0200

    Introduced two new Reports: 1. Customer services report 2. leasing services report

---
+++ production_changes/pending/changeset28.sql
@@ -0,0 +1,182 @@
+﻿-- Function: system.is_not_manager(character varying)
+
+-- DROP FUNCTION system.is_not_manager(character varying);
+
+CREATE OR REPLACE FUNCTION system.is_not_manager(character varying)
+  RETURNS boolean AS
+$BODY$
+declare
+	user_id varchar;
+begin
+	select id into user_id from system.appuser where username = $1;
+
+	if user_id in (select appuser_id from system.appuser_appgroup where appgroup_id = 'manager-id') then
+		return false;
+	else 
+		return true;
+	end if;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION system.is_not_manager(character varying)
+  OWNER TO postgres;
+
+-- Function: system.is_manager(character varying)
+
+-- DROP FUNCTION system.is_manager(character varying);
+
+CREATE OR REPLACE FUNCTION system.is_manager(character varying)
+  RETURNS boolean AS
+$BODY$
+declare
+	user_id varchar;
+begin
+	select id into user_id from system.appuser where username = $1;
+
+	if user_id in (select appuser_id from system.appuser_appgroup where appgroup_id = 'manager-id') then
+		return true;
+	else 
+		return false;
+	end if;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION system.is_manager(character varying)
+  OWNER TO postgres;
+
+-- Function: system.is_lease_coord(character varying)
+
+-- DROP FUNCTION system.is_lease_coord(character varying);
+
+CREATE OR REPLACE FUNCTION system.is_lease_coord(character varying)
+  RETURNS boolean AS
+$BODY$
+
+begin
+	
+	if $1 in (select appuser_id from system.appuser_appgroup where appgroup_id = 'lease-id') then
+		return true;
+	else 
+		return false;
+	end if;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION system.is_lease_coord(character varying)
+  OWNER TO postgres;
+
+-- Function: system.is_customer_rep(character varying)
+
+-- DROP FUNCTION system.is_customer_rep(character varying);
+
+CREATE OR REPLACE FUNCTION system.is_customer_rep(character varying)
+  RETURNS boolean AS
+$BODY$
+
+begin
+	
+	if $1 in (select appuser_id from system.appuser_appgroup where appgroup_id = 'cust-reps-id') 
+	and $1 not in (select appuser_id from system.appuser_appgroup where appgroup_id = 'manager-id') then
+		return true;
+	else 
+		return false;
+	end if;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION system.is_customer_rep(character varying)
+  OWNER TO postgres;
+
+DROP FUNCTION IF EXISTS application.customer_services_report( date, date);
+CREATE OR REPLACE FUNCTION application.customer_services_report( date, date)
+  RETURNS TABLE(application varchar, lodged int, queried int, awaiting_collection int, collected int) AS
+$BODY$ 
+DECLARE
+
+BEGIN
+return query
+
+	WITH app_lodged AS (with requests as (
+	select ser.id service_id, application_id, ser.request_type_code request_type, ser.status_code service_status, app.status_code application_status,
+	ser.change_user service_user, app.change_user app_user, app.stage_code app_stage, app.lodging_datetime, app.change_time
+	from application.service as ser 
+	inner join application.application as app on ser.application_id = app.id 
+	where ser.status_code = 'lodged' and app.status_code = 'lodged'
+	and app.lodging_datetime BETWEEN $1 and $2 
+	order by ser.request_type_code)
+	select request.display_value, count(*) as application_count
+	from requests 
+	inner join application.request_type as request on requests.request_type = request.code group by request_type ,request.display_value),
+
+	app_queried AS (with requests as (
+	select ser.id service_id, application_id, ser.request_type_code request_type, ser.status_code service_status, app.status_code application_status,
+	ser.change_user service_user, app.change_user app_user, app.stage_code app_stage, app.lodging_datetime, app.change_time
+	from application.service as ser 
+	inner join application.application as app on ser.application_id = app.id 
+	where system.is_manager(app.change_user) and system.is_customer_rep(app.assignee_id) 
+	and app.status_code = 'lodged' and ser.status_code = 'lodged'
+	and app.lodging_datetime BETWEEN $1 and $2 
+	order by ser.request_type_code)
+	select request.display_value, count(*) as application_count
+	from requests 
+	inner join application.request_type as request on requests.request_type = request.code group by request_type ,request.display_value), 
+
+	
+	collection AS (with requests as (
+	select ser.id service_id, application_id, ser.request_type_code request_type, ser.status_code service_status, app.status_code application_status,
+	ser.change_user service_user, app.change_user app_user, app.stage_code app_stage, app.lodging_datetime, app.change_time
+	from application.service as ser 
+	inner join application.application as app on ser.application_id = app.id 
+	where app.status_code = 'approved' and app.stage_code = 'collection' 
+	and app.lodging_datetime BETWEEN $1 and $2 
+	order by ser.request_type_code)
+	select request.display_value, count(*) as application_count
+	from requests 
+	inner join application.request_type as request on requests.request_type = request.code group by request_type ,request.display_value),
+
+	collected AS (with requests as (
+	select ser.id service_id, application_id, ser.request_type_code request_type, ser.status_code service_status, app.status_code application_status,
+	ser.change_user service_user, app.change_user app_user, app.stage_code app_stage, app.lodging_datetime, app.change_time
+	from application.service as ser inner join application.application as app on ser.application_id = app.id 
+	where app.status_code = 'completed'  
+	and app.lodging_datetime BETWEEN $1 and $2 
+	order by ser.request_type_code)
+	select request.display_value, count(*) as application_count
+	from requests 
+	inner join application.request_type as request on requests.request_type = request.code group by request_type ,request.display_value)
+	
+	select distinct rt.display_value, (
+	select application_count from app_lodged 
+	where app_lodged.display_value = rt.display_value)::int as lodged,
+	(select application_count from app_queried 
+	where app_queried.display_value = rt.display_value)::int as queried,
+	(select application_count from collection 
+	where collection.display_value = rt.display_value)::int as awaiting_collection,
+	(select application_count from collected 
+	where collected.display_value = rt.display_value)::int as collected
+	from application.request_type as rt, application.service as ser 
+	where rt.code = ser.request_type_code and rt.request_category_code in ('registrationServices','leaseServices') 
+	order by rt.display_value; 
+      
+
+           
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE;
+
+ select * from application.customer_services_report( '2014-01-02', '2014-03-21') ;
+
+
+  /*select * from application.service ser, application.application app where ser.application_id = app.id and ser.request_type_code = 'consentApplication' 
+  and app.stage_code='appCorrection' --and app.id not in (select id from application.application_historic)
+*/
+
+
+
+
+
+  
---
+++ production_changes/pending/changeset30.sql
@@ -0,0 +1,119 @@
+﻿--Table application.application_stage_type ----
+
+UPDATE application.application
+SET stage_code = NULL
+WHERE stage_code <> NULL;
+
+DROP TABLE IF EXISTS system.appstage_appgroup;
+DROP TABLE IF EXISTS application.application_stage_type CASCADE;
+CREATE TABLE application.application_stage_type(
+    code varchar(20) NOT NULL,
+    display_value varchar(255) NOT NULL,
+    status char(1) NOT NULL,
+    description varchar(555),
+
+    -- Internal constraints
+    
+    CONSTRAINT application_stage_type_display_value_unique UNIQUE (display_value),
+    CONSTRAINT application_stage_type_pkey PRIMARY KEY (code)
+);
+
+
+comment on table application.application_stage_type is '';
+    
+ -- Data for the table application.application_stage_type -- 
+insert into application.application_stage_type(code, display_value, status, description) values('newApp', 'New application lodged', 'c', 'Application awaiting approval for processing');
+insert into application.application_stage_type(code, display_value, status, description) values('appApprove', 'Application awaiting approval', 'c', 'All services in the application have been completed and the application can now be approved');
+insert into application.application_stage_type(code, display_value, status, description) values('appProcess', 'Application to be processed', 'c', 'The application has been lodged and is ready to be processed');
+insert into application.application_stage_type(code, display_value, status, description) values('appInfoIncorrect', 'Application sent back to correct input information', 'c', 'Processed application has incomplete or incorrect information and cannot be approved');
+insert into application.application_stage_type(code, display_value, status, description) values('missingPlot', 'Application sent back because of missing plot', 'c', 'Application cannot be processed because of missing plot. Awaiting response from Survey and Mapping department');
+insert into application.application_stage_type(code, display_value, status, description) values('areaMismatch', 'Application sent back to correct plot area mismatch', 'c', 'Application cannot be processed because the cadastral area in the system is different from the area on the S10 Form');
+insert into application.application_stage_type(code, display_value, status, description) values('custCollect', 'Enter collection details', 'c', 'Enter details of the person who collects the completed, approved application');
+insert into application.application_stage_type(code, display_value, status, description) values('callCustomer', 'Customer Services to call customer for signature', 'c', 'The customer is called to sign the draft');
+insert into application.application_stage_type(code, display_value, status, description) values('custSign', 'Customer to sign draft', 'c', 'Lease draft awaits customerâ€™s signature');
+insert into application.application_stage_type(code, display_value, status, description) values('executiveSign', 'Executive Office to sign lease draft', 'c', 'The lease draft shall be signed by the Director General');
+insert into application.application_stage_type(code, display_value, status, description) values('bindDraft', 'Leasing Services to bind lease draft', 'c', 'The lease draft shall now be bound and made ready for registration');
+insert into application.application_stage_type(code, display_value, status, description) values('checkDraft', 'Director of Lease Services to check draft', 'c', 'The signed lease draft is ready to be checked and the application approved');
+insert into application.application_stage_type(code, display_value, status, description) values('logDraft', 'Leasing Services to Log ready drafts', 'c', 'The application is ready to be logged as completed and approved for registration');
+insert into application.application_stage_type(code, display_value, status, description) values('appRegister', 'Application ready for registration', 'c', 'The lease application is ready to be registered');
+insert into application.application_stage_type(code, display_value, status, description) values('appArchive', 'Application ready for archiving', 'c', 'The application is ready to be archived');
+
+------------
+
+-- Table: system.appstage_appgroup
+
+DROP TABLE system.appstage_appgroup;
+
+CREATE TABLE system.appstage_appgroup
+(
+  appstage_code character varying(40) NOT NULL,
+  appgroup_id character varying(40) NOT NULL,
+  CONSTRAINT appstage_appgroup_pkey PRIMARY KEY (appstage_code , appgroup_id ),
+  CONSTRAINT appstage_appgroup_appgroup_id_fk142 FOREIGN KEY (appgroup_id)
+      REFERENCES system.appgroup (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT appstage_appgroup_appstage_code_fk141 FOREIGN KEY (appstage_code)
+      REFERENCES application.application_stage_type (code) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE system.appstage_appgroup
+  OWNER TO postgres;
+COMMENT ON TABLE system.appstage_appgroup
+  IS 'This many-to-many table contains users, related to groups. Allows to have multiple groups for one user.';
+
+-- Index: system.appstage_appgroup_appgroup_id_fk142_ind
+
+-- DROP INDEX system.appstage_appgroup_appgroup_id_fk142_ind;
+
+CREATE INDEX appstage_appgroup_appgroup_id_fk142_ind
+  ON system.appstage_appgroup
+  USING btree
+  (appgroup_id COLLATE pg_catalog."default" );
+
+-- Index: system.appstage_appgroup_appstage_code_fk141_ind
+
+-- DROP INDEX system.appstage_appgroup_appstage_code_fk141_ind;
+
+CREATE INDEX appstage_appgroup_appstage_code_fk141_ind
+  ON system.appstage_appgroup
+  USING btree
+  (appstage_code COLLATE pg_catalog."default" );
+
+
+ -- Data for the table system.appstage_appgroup -- 
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('newApp', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('newApp', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('newApp', 'surveymapp-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appApprove', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appApprove', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appApprove', 'surveymapp-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appApprove', 'manager-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appProcess', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appProcess', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appProcess', 'lease-correct-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appProcess', 'surveymapp-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appArchive', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appArchive', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appArchive', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appArchive', 'surveymapp-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appInfoIncorrect', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appInfoIncorrect', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appInfoIncorrect', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appInfoIncorrect', 'surveymapp-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('missingPlot', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('areaMismatch', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('custCollect', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('custCollect', 'deeds-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('callCustomer', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('custSign', 'cust-reps-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('executiveSign', 'dg-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('bindDraft', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('checkDraft', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('logDraft', 'lease-id');
+INSERT INTO system.appstage_appgroup(appstage_code, appgroup_id) VALUES ('appRegister', 'deeds-reps-id');
+
+
+
